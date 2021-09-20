fc3a33d65f7c9eb1388534a105ef7af6adc96b5f
gitinun55 <54529430+gitinun55@users.noreply.github.com>
2021-09-20 17:41:42 +0700

    SoureCode

---
+++ index.php
@@ -0,0 +1,269 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Test AppDD</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="" />
+
+  <link rel="stylesheet" href="css/styles.css" />
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+  <link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.min.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js"></script>
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+  <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+  <!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/perfect-scrollbar/perfect-scrollbar.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="css/util.css">
+<!--===============================================================================================-->
+</head>
+<!-- End Head -->
+
+<body>
+  <div class="navbar">
+    <div class="navbar-container">
+      <div class="logo">
+        <img class="logo" src="image/logo-gfmis.png" alt="logo" width="100" height="45">
+      </div>
+
+      <nav>
+          <ul>
+            <li><a href="index.php">หน้าหลัก</a></li>
+            <li><a href="#">หลักสูตรออนไลน์</a></li>
+            <li><a href="#">ข่าวสาร</a></li>
+            <li><a href="#">ถ่ายทอดสด</a></li>
+            <li><a href="#">กระดานสนทนา</a></li>
+            <li><a href="#">คำถามที่พบบ่อย</a></li>
+            <li><a href="#">ติดต่อเรา</a></li>
+          </ul>
+      </nav>
+
+        <form class="form-inline  ">
+          <input class="form-control" type="search" placeholder="  Search.." aria-label="Search..">
+        </form>
+        <div class="button">
+          <a href="">เข้าสู่ระบบ</a>
+        </div>
+    </div>
+  </div>
+  <!-- End Navbar -->
+  
+  <div class="body-container">
+    <div class="carousel" data-flickity='{ "wrapAround": true, "autoPlay": true, "imagesLoaded":true }'>
+      <div class="carousel-cell">
+        <img class="w3-image" src="https://smash-images.photobox.com/original/5f04c1b41fd48d1b10ff27dfc90548bf13608845_Large-Print-lifestyle-3_1-2600.jpg">
+      </div>
+      <div class="carousel-cell">
+        <img class="w3-image" src="https://smash-images.photobox.com/original/bca8e5fa7862a2cfaefc300c5b572e7a6dc6f3f3_Standard-Prints-lifestyle-3_1-2600.jpg">
+      </div>
+      <div class="carousel-cell">
+        <img class="w3-image" src="https://smash-images.photobox.com/original/a422aed1a721e933961b19ea9e47e07fc71e0699_Acrylic-Prints-lifestyle-3_1-2600.jpg">
+      </div>
+    </div>
+    <!-- End Carousel -->
+
+    <div class="col">
+      <div class="list">
+        <h1>หลักสูตรออนไลน์</h1>
+      </div>
+    </div>
+
+    <div class="dec">
+      <div class="row">
+        <div class="col"><img src="image/1.png" class="img-circle" alt="Cinque Terre" width="150" height="150">
+          <p>ภาพรวมระบบสารสนเทศการ<br>
+              เงินการคลังเพื่อรองรับ</br>
+              องค์กรปกครองส่วนท้องถื่น</br>
+          </p>
+        </div>
+        <div class="col"><img src="image/2.png" class="img-circle" alt="Cinque Terre" width="150" height="150">
+          <p>ระบบงบประมาณ</p>
+        </div>
+        <div class="col"><img src="image/3.jpg" class="img-circle" alt="Cinque Terre" width="150" height="150">
+          <p>ระบบจัดซื้อจัดจ้าง</p>
+        </div>
+        <div class="col"><img src="image/4.jpg" class="img-circle" alt="Cinque Terre" width="150" height="150">
+          <p>ระบบเบิกจ่าย</p>
+        </div>
+        <div class="col"><img src="image/10.jpg" class="img-circle" alt="Cinque Terre" width="150" height="150">
+          <p>ระบบบัญชีแยกประเภท</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="dec">
+      <div class="row">
+        <div class="col"><img src="image/6.jpg" class="img-circle" alt="Cinque Terre" width="200" height="200">
+          <p>ระบบข้อมูลเพื่อผู้บริหาร</p>
+        </div>
+        <div class="col"><img src="image/7.jpg" class="img-circle" alt="Cinque Terre" width="200" height="200">
+          <p>การบูรณาการและการเชื่อมโยง</p>
+        </div>
+        <div class="col"><img src="image/8.jpg" class="img-circle" alt="Cinque Terre" width="200" height="200">
+          <p>การใช้ซอฟต์แวร์เครื่องมือ<br>
+              ระบบวิเคราะห์
+          </p>
+        </div>
+        <div class="col"><img src="image/9.jpg" class="img-circle" alt="Cinque Terre" width="200" height="200">
+          <p>ผู้ดูแลระบบสารสนเทศการ<br>
+            เงินการคลังเพื่อรองรับ<br>
+            องค์กรปกครองส่วนท้องถื่น<br>
+            (System Administration)
+          </p>
+        </div>
+        <div class="col"><img src="image/5.jpg" class="img-circle" alt="Cinque Terre" width="200" height="200">
+          <p>การบริหารฐานข้อมูล<br>
+            (Database Administration)
+          </p>
+        </div>
+      </div>
+    </div>
+    <!-- End Col -->
+    
+		<div class="container-table100">
+			<div class="wrap-table100">
+				<div class="table100 ver1 m-b-110">
+					<div class="table100-head">
+						<table>
+							<thead>
+								<tr class="row100 head">
+									<th class="column1">UserId</th>
+									<th class="column2">id</th>
+									<th class="column3">title</th>
+									<th class="column4">body</th>
+								</tr>
+							</thead>
+						</table>
+					</div>
+
+					<div class="table100-body js-pscroll">
+						<table>
+							<tbody id = "ajaxTest"></tbody>
+								<td class="column_data1"></td>
+								<td class="column_data2"></td>
+								<td class="column_data3"></td>
+								<td class="column_data4"></td>
+						</table>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+  </div>
+  <!-- End body -->
+
+  <footer>
+    <div class="container-footer">
+      <div class="footer-logo">
+        <img class="logo" src="image/logo-gfmis.png" alt="logo" width="110" height="45">
+      </div>
+
+      <div class="name">
+        <h3>ศูนย์เทคโนโลยีสารสนเทศและการสื่อสาร สำนักงานปลัดกระทรวงการคลัง กระทรวงการคลัง</h3>
+        <p>ถนนพระราม 6 แขวงพญาไท เขตพญาไท กรุงเทพฯ 10400<br>
+        โทรศัพท์ 02-126-5800<br>
+        โทรสาร 02-273-9408<br> 
+        <ul class="socials">
+          <li><a href="#"><i class="fab fa-facebook"></i></a></li>
+          <li><a href="#"><i class="fab fa-youtube"></i></a></li>
+        </ul>     
+        </p>
+      </div>
+
+      <div class="display">
+        <h5>แผนผังเว็บไซต์</h5>
+        <ul>
+          <li><a href="index.php">หน้าหลัก</a></li>
+          <li><a href="#">หลักสูตรออนไลน์</a></li>
+          <li><a href="#">ข่าวสาร</a></li>
+          <li><a href="#">ถ่ายทอดสด</a></li>
+          <li><a href="#">กระดานสนทนา</a></li>
+          <li><a href="#">คำถามที่พบบ่อย</a></li>
+          <li><a href="#">ติดต่อเรา</a></li>
+        </ul>     
+        </p>
+      </div>
+    </div>
+  </footer>
+
+  <div class="footer-bottom">
+    <p>copyright &copy;2021 All Rights Reserved. newgfmis.com Designed by <span>Kitinun Khonson</span></p>
+  </div>
+  <!-- End Footer -->
+
+  <!--===============================================================================================-->	
+	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+  <!--===============================================================================================-->
+	<script src="vendor/bootstrap/js/popper.js"></script>
+	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+  <!--===============================================================================================-->
+	<script src="vendor/select2/select2.min.js"></script>
+  <!--===============================================================================================-->
+	<script src="vendor/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+	<script>
+		$('.js-pscroll').each(function(){
+			var ps = new PerfectScrollbar(this);
+
+			$(window).on('resize', function(){
+				ps.update();
+			})
+		});
+	</script>
+<!--===============================================================================================-->
+	<script src="js/main.js"></script>
+</body>
+
+</html>
+
+<script>
+
+	var htmlBody;
+	htmlBody = '<table class="border: 1px solid black;">'; /** เปิด Table **/
+	$(document).ready(function(){
+	$.ajax({ /** XHR **/
+		type:"GET",
+		url:"https://jsonplaceholder.typicode.com/posts",
+		success:function(body){
+		console.log(body);
+		for(var index = 0 ; index < 10 ; index++){
+			htmlBody += "<tr>";
+			htmlBody += "<td>"+ body[index]['userId'] + "<td/>"; 
+			htmlBody += "<td>"+ body[index]['id'] + "<td/>";
+			htmlBody += "<td>"+ body[index]['title'] + "<td/>"; 
+			htmlBody += "<td>"+ body[index]['body'] + "<td/>"; 
+			htmlBody += "</tr>";
+		}
+		htmlBody += "</table>"; /** ปิด Table **/
+		$('#ajaxTest').html(htmlBody); /** Draw as HTML **/ 
+		$('.test').html(htmlBody);
+		
+		}
+	});
+	});
+
+</script>
